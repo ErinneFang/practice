fd133e6312dbb8ce8ea3f574fb574dd4ddacb30a
ErinneFang <fang00328@gmail.com>
2021-03-03 19:34:13 +0800

    no message

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,9 +2,7 @@ public class HelloWorld {
 
     public static void main(String[] args) {
 
-        System.out.println("Hello World~~");
-        aaaaa
-        11111
+        System.out.println("Hello World~~!!");
 
 
     }
